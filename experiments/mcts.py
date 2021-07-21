d5ba3c782b5b3d13319620c0431caf3e04c90e5f
Anonymous-author-code <73868935+Anonymous-author-code@users.noreply.github.com>
2021-07-21 20:33:18 +0800

    Create mcts.py

---
+++ experiments/mcts.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def run(args):
+
+	n, d = args.N, args.D
+	points = np.random.rand(n, d)
+	tsp = alphatsp.tsp.TSP(n, d, points=points)
+
+	# MCTS Solver
+	mcts_solver = alphatsp.solvers.mcts.MCTSSolver(args, tsp)
+	mcts_sol, mcts_sol_length = mcts_solver.solve()
+	print(f"MCTS Tour:\t{mcts_sol_length},\t{mcts_sol}")
+
+	# Nearest neighbor greedy solver
+	greedy_sol, greedy_sol_length = alphatsp.solvers.heuristics.nearest_greedy(tsp)
+	print(f"Greedy Tour:\t{greedy_sol_length},\t{greedy_sol}")
+
+	# Exact solver
+	sol, sol_length = alphatsp.solvers.exact.exact(tsp)
+	print(f"Exact solution:\t{sol_length},\t{sol}")
